cf16474d4d087cc28d0010edc4f28c1646154a0a
Muriithi Ian <imuriithiian@gmail.com>
2023-08-29 21:29:24 +0300

    Fixed home url

--- KRA_DC_Access_pass/settings.py
+++ KRA_DC_Access_pass/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'formtools',
+    'crispy_forms',
     'access_pass',
 ]
 
